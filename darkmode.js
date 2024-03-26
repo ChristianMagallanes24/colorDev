71bb6afe638cec3952303aea58669ccd04a20edc
Christian magallanes <desarrollowebfront@gmail.com>
2024-03-26 04:54:41 -0300

    ajustes

--- darkmode.js
+++ darkmode.js
@@ -4,8 +4,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const bodyElement = document.querySelector("body"); // Seleccionar el elemento body
     const tituloElement = document.getElementById("titulo"); // Corregir la selección del elemento del título
 
+    let modoOscuroActivado = false; // Variable para controlar el estado del modo oscuro
+
     function cambiarColor() {
+        modoOscuroActivado = !modoOscuroActivado; // Alternar el estado del modo oscuro al hacer clic en el botón
         bodyElement.classList.toggle("dark");
         tituloElement.classList.toggle("dark-titulo"); // Usar toggle en lugar de add para alternar la clase
+
+        // Cambiar el texto del botón según el estado del modo oscuro
+        if (modoOscuroActivado) {
+            darkMode.innerText = "Light Mode";
+        } else {
+            darkMode.innerText = "Dark Mode";
+        }
     }
 });
