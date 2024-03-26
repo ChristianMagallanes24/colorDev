98b614a621ffc245456037248ffec92ef46e294d
Christian magallanes <desarrollowebfront@gmail.com>
2024-03-26 04:02:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btnGenerar = document.getElementById("btnGenerar");
+    const paleta = document.getElementById("paleta");
+    let colorCopiado = document.getElementById("colorCopiado");
+
+    function generarColor() {
+        const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        return color;
+    }
+
+    function actualizarColor() {
+        document.querySelectorAll(".paleta > div").forEach(function(colorDiv) {
+            const color = generarColor();
+            colorDiv.style.backgroundColor = color;
+
+            // Agregar evento de clic para copiar el color al portapapeles
+            colorDiv.addEventListener("click", function() {
+                navigator.clipboard.writeText(color).then(function() {
+                    console.log("Color copiado: " + color);
+                    colorCopiado.innerText = "color copiado: " + color;
+                    colorCopiado.style.color = color; // Aplicar el color copiado como color del texto
+                }).catch(function(err) {
+                    console.error("Error al copiar el color: ", err);
+                });
+            });
+        });
+    }
+
+    btnGenerar.addEventListener("click", function() {
+        actualizarColor();
+    });
+
+    actualizarColor();
+});
